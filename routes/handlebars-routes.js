0a3481ddd787e12ce3392b5eb7083942844db8a0
anniemcfarland7 <anniemcfarland@aol.com>
2021-02-25 20:06:02 -0600

    updating

--- routes/handlebars-routes.js
+++ routes/handlebars-routes.js
@@ -3,14 +3,16 @@ const db = require("../models/index.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
   db.Event.findAll({})
     .then(data => {
-      console.log(data);
-      res.render("index", { db: data });
+      const events = data.map(event => ({
+        event_name: event.event_name,
+        event_location: event.event_location
+      }));
+      console.log(events);
+      res.render("index", { events: events });
     })
     .catch(err => console.log(err));
-
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,26 +13,22 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-
-// const routes = require("./controllers/rendevoodle2_controller.js");
-
-const hbRouter = require("./routes/handlebars-routes.js");
-
-app.use("/", hbRouter);
-
 // apiRouter(app);
 // app.use(routes);
 
-
 // Static directory
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Invoke routes
 app.use("/", hbRouter);
 // authorRouter(app);
 // apiRouter(app);
 
+// const db = require("./models");
+
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+app.listen(PORT, () => {
+  return console.log(`App listening on: http://localhost:${PORT}`);
 });
