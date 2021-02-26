838b648114d082cfd8f9cef50a4e9496e5f227a2
Annie McFarland <73352291+anniemcfarland7@users.noreply.github.com>
2021-02-25 19:30:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 const db = require("./models");
+const hbRouter = require("./routes/handlebars-routes.js");
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -21,11 +28,15 @@ app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 // Invoke routes
 // htmlRouter(app);
 // authorRouter(app);
 // apiRouter(app);
+app.use("/", hbRouter);
 
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -37,6 +48,8 @@ require("./routes/html-routes.js")(app);
 require("./routes/admin-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    return console.log(`App listening on: http://localhost:${PORT}`);
+  });
 });
