7e7ff7aee1726fee73fa3caa35785874703b00f5
Audrey King <audreymking1@gmail.com>
2021-02-25 21:52:22 -0600

    updated route/sql data to add images

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO events (event_name, event_location) VALUES ('St. Louis Zoo', 'https://www.stlzoo.org/');
-INSERT INTO events (event_name, event_location) VALUES ('Gateway Arch National Park', 'https://www.nps.gov/jeff/index.htm');
-INSERT INTO events (event_name, event_location) VALUES ('Six Flags', 'https://www.sixflags.com/stlouis');
-INSERT INTO events (event_name, event_location) VALUES ('Union Station', 'https://www.stlouisunionstation.com/');
-INSERT INTO events (event_name, event_location) VALUES ('City Museum', 'https://www.citymuseum.org/');
+INSERT INTO events (event_name, event_location, event_image) VALUES ('St. Louis Zoo', 'https://www.stlzoo.org/', 'https://explorestlouis.com/wp-content/uploads/2020/08/Saint-Louis-Zoo-Polar-Bear.jpg');
+INSERT INTO events (event_name, event_location, event_image) VALUES ('Gateway Arch National Park', 'https://www.nps.gov/jeff/index.htm', 'https://explorestlouis.com/wp-content/uploads/2020/08/Gateway-Arch-Website-Listing-3.jpg');
+INSERT INTO events (event_name, event_location, event_image) VALUES ('Six Flags', 'https://www.sixflags.com/stlouis', 'https://explorestlouis.com/wp-content/uploads/2020/08/Six-Flags-Web-2.jpg');
+INSERT INTO events (event_name, event_location, event_image) VALUES ('Union Station', 'https://www.stlouisunionstation.com/', 'https://d3qvqlc701gzhm.cloudfront.net/full/90020cd806468d1f8d69890dc3258bccbd53605ff6b0354b72cf480e55cb2620.jpg');
+INSERT INTO events (event_name, event_location, event_image) VALUES ('City Museum', 'https://www.citymuseum.org/','https://d3qvqlc701gzhm.cloudfront.net/full/9ba2c4735e6a9ae2b9d6aedc48426743efe9987450f5f4b5a4c2018ac1feba60.jpg');
 
 
 INSERT INTO reviews (party_name, client_comment, client_rate) VALUES ('St. Louis Zoo Trip', 'Fun for all ages!', 5);
--- models/events.js
+++ models/events.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     {
       //Giving the Reviewer model a name of type STRING
       event_name: DataTypes.STRING,
-      event_location: DataTypes.STRING
+      event_location: DataTypes.STRING,
+      event_image: DataTypes.STRING
     },
     {
       timestamps: false
