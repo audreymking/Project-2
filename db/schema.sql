bea2b796616114421cf85c99dd03f279f4d53577
Bob Nguyen <mrduynguyen235@gmail.com>
2021-02-27 10:41:54 -0600

    fix admin

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
 ### Schema
-
 CREATE DATABASE events_db;
 USE events_db;
 
@@ -8,7 +7,7 @@ CREATE TABLE events
 	id int NOT NULL AUTO_INCREMENT,
 	event_name varchar(255) NOT NULL,
     /* for event URL's */
-    event_location varchar(255) NOT NULL, 
+  event_location varchar(255) NOT NULL, 
 	event_image varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
@@ -23,5 +22,14 @@ CREATE TABLE reviews
 	client_comment varchar(255) NOT NULL,
     /* scale of 1-5 stars for this event */
 	client_rate int NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
