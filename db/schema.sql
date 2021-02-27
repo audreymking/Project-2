2eb37cb2bc429e3400b5e48e796f197cfd41c52a
Audrey King <audreymking1@gmail.com>
2021-02-27 12:11:16 -0600

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
 ### Schema
-
 CREATE DATABASE events_db;
 USE events_db;
 
@@ -8,7 +7,7 @@ CREATE TABLE events
 	id int NOT NULL AUTO_INCREMENT,
 	event_name varchar(255) NOT NULL,
     /* for event URL's */
-    event_location varchar(255) NOT NULL, 
+  	event_location varchar(255) NOT NULL, 
 	event_image varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
@@ -17,11 +16,18 @@ CREATE TABLE events
 CREATE TABLE reviews
 (
 	id int NOT NULL AUTO_INCREMENT,
-    /* event client attended */
-	party_name varchar(255) NOT NULL,
     /* text box space for comments */
 	client_comment varchar(255) NOT NULL,
     /* scale of 1-5 stars for this event */
-	client_rate int NOT NULL,
+	client_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
